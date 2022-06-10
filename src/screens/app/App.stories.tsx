294c1ab5002103c881badb1e1fb3c4714d05a104
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-06-09 23:43:52 -0400

    Add failing App test

--- src/screens/app/App.stories.tsx
+++ src/screens/app/App.stories.tsx
@@ -1,4 +1,6 @@
 import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
 import App from "./App";
 
@@ -12,3 +14,13 @@ export default meta;
 type Story = ComponentStoryObj<typeof App>;
 
 export const Standard: Story = {};
+
+export const SuggestionError: Story = {
+  play: async ({ canvasElement }) => {
+    const { getByLabelText, getByRole } = within(canvasElement);
+    const input = getByLabelText("Proposed event topic");
+    await userEvent.type(input, "uh");
+    await userEvent.click(getByRole("button", { name: "Submit" }));
+    await expect(input).toHaveAccessibleDescription("Can we have some more detail?");
+  },
+};
